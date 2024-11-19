1d286c10448bc6b52d08ba754fb6d854350b666d
saideeWD <mdsaideehasan@gmail.com>
2024-11-19 10:45:26 +0600

    Register section problem

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../provider/AuthProvider';
 
 export default function Register() {
-  const [username, setUsername] = useState('');
+  const [username, setUsernam] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
