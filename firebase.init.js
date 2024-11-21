38c217460c2feeeddf86ad6bf0b283522f8410e0
saideeWD <mdsaideehasan@gmail.com>
2024-11-21 20:27:27 +0600

    enV

--- firebase.init.js
+++ firebase.init.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnB41FUjvAelr2J_OyqOJTXhGFJEegeeQ",
-  authDomain: "problem-8f5f0.firebaseapp.com",
-  projectId: "problem-8f5f0",
-  storageBucket: "problem-8f5f0.firebasestorage.app",
-  messagingSenderId: "937104619175",
-  appId: "1:937104619175:web:1a9cdf238a43ebdc948d82",
-  measurementId: "G-8DSJ56WS75"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
+  measurementId:import.meta.env.VITE_measurementId,
 };
 
 
